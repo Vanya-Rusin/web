dd9990e3a814bac5bdd05bd12f2f879b01508f40
Vanya <74555104+Vanya-Rusin@users.noreply.github.com>
2021-10-22 12:15:10 +0300

    Add files via upload

--- mkr/zavd 2.js
+++ mkr/zavd 2.js
@@ -1,8 +1,8 @@
 function compareNumber(a,b)
 {
-    if(a>b) return 1;
+    if(a<b) return 1;
     if (a===b) return 0;
-    if(a<b) return -1;
+    if(a>b) return -1;
 
 }
 console.log(randomArray.sort(compareNumber));
